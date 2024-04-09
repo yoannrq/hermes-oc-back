fb7507b70da585193e0fc29f0c621ac81beb9dfc
Yoann Rouquie <rouquieyoann@protonmail.com>
2024-04-09 23:24:12 +0200

    feat: Patch last message read by the current user.

--- src/controllers/meController.js
+++ src/controllers/meController.js
@@ -2,6 +2,7 @@ import bcrypt from 'bcrypt';
 import postgresClient from '../models/postgresClient.js';
 import userSchema from '../utils/validation/userSchema.js';
 import formatingName from '../utils/formatingName.js';
+import mongoClient from '../models/mongoClient.js';
 
 export default {
   getMe: async (req, res) => {
@@ -52,6 +53,67 @@ export default {
       res.locals.user = updatedUser;
 
       return res.status(200).json(updatedUser);
+    } catch (err) {
+      return next({
+        status: 400,
+        message: 'Bad request',
+        error: err,
+      });
+    }
+  },
+
+  updateLastMessageRead: async (req, res, next) => {
+    const { user } = res.locals;
+    const {
+      conversationId, channelId, teamId, messageId,
+    } = req.body;
+
+    if (!messageId) {
+      return res.status(200).json({ message: 'No messages' });
+    }
+
+    if (!conversationId && !channelId && !teamId) {
+      return next({
+        status: 400,
+        message: 'Missing conversationId, channelId or teamId',
+      });
+    }
+
+    if ((conversationId && channelId) || (conversationId && teamId) || (channelId && teamId)) {
+      return next({
+        status: 400,
+        message: 'You must provide only one of conversationId, channelId or teamId',
+      });
+    }
+
+    try {
+      const lastMessageRead = await mongoClient.lastMessageRead.findFirst({
+        where: {
+          AND: [
+            { readerId: parseInt(user.id, 10) },
+            { conversationId: parseInt(conversationId, 10) },
+          ],
+        },
+      });
+
+      if (lastMessageRead) {
+        await mongoClient.lastMessageRead.update({
+          where: { id: lastMessageRead.id },
+          data: {
+            messageId,
+          },
+        });
+      } else {
+        await mongoClient.lastMessageRead.create({
+          data: {
+            messageId,
+            readerId: parseInt(user.id, 10),
+            conversationId: parseInt(conversationId, 10),
+          },
+        });
+      }
+
+      return res.status(200).json({ message: 'Last message read updated' });
     } catch (err) {
       return next({
         status: 400,
--- src/routers/meRouter.js
+++ src/routers/meRouter.js
@@ -56,4 +56,55 @@ router.get('/', meController.getMe);
  */
 router.patch('/', meController.updateMe);
 
+/**
+ * @swagger
+ * /api/me/last-message-read:
+ *   patch:
+ *     summary: Update the last message read for a user in a conversation, channel, or team
+ *     description: |
+ *       Updates the last read message for a user, specifying only one of conversationId, channelId, or teamId.
+ *       These three parameters are mutually exclusive – only one of them should be provided.
+ *     tags:
+ *       - me
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - messageId
+ *             properties:
+ *               messageId:
+ *                 type: string
+ *                 description: The ID of the last message that has been read.
+ *               conversationId:
+ *                 type: integer
+ *                 description: The ID of the conversation to update the last message read, mutually exclusive with channelId and teamId.
+ *               channelId:
+ *                 type: integer
+ *                 description: The ID of the channel to update the last message read, mutually exclusive with conversationId and teamId.
+ *               teamId:
+ *                 type: integer
+ *                 description: The ID of the team to update the last message read, mutually exclusive with conversationId and channelId.
+ *     responses:
+ *       200:
+ *         description: Last message read updated successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 'Last message read updated'
+ *       400:
+ *         description: Bad request - missing or incorrect parameters.
+ *       401:
+ *         description: Unauthorized.
+ */
+router.patch('/last-message-read', meController.updateLastMessageRead);
+
 export default router;
