d90fa9728f5d95bd475c3aeddcaa102ecffedfdb
JrmLg <63566435+JrmLg@users.noreply.github.com>
2024-04-13 17:20:43 +0200

    fix: Prevents conversation duplication between two users.

--- src/controllers/conversationController.js
+++ src/controllers/conversationController.js
@@ -39,69 +39,71 @@ export default {
       }));
 
       // Parcours chaque conversation pour enrichir les données avec le dernier message et le nombre de messages non lus
-      const conversationsWithMessages = await Promise.all(formatedUserConversations.map(async (conversation) => {
-        // Récupère le dernier message de la conversation courante
-        const lastMessage = await mongoClient.message.findMany({
-          where: {
-            conversationId: conversation.conversationid,
-          },
-          orderBy: {
-            id: 'desc', // Tri par ordre décroissant pour obtenir le dernier message
-          },
-          take: 1, // Prend seulement le premier résultat qui est le dernier message
-        });
-
-        // Recherche le dernier message lu par l'utilisateur dans cette conversation
-        const lastMessageRead = await mongoClient.lastMessageRead.findFirst({
-          where: {
-            conversationId: conversation.conversationid,
-            readerId: user.id, // Filtre par ID de l'utilisateur pour trouver son dernier message lu
-          },
-        });
-
-        // Initialise le compteur de messages non lus
-        let unreadMessagesCount = 0;
-        if (lastMessageRead) {
-          // Si un dernier message lu est trouvé, compte les messages non lus depuis ce dernier message
-          const lastReadMessageId = lastMessageRead.messageId;
-
-          unreadMessagesCount = await mongoClient.message.count({
-            where: {
-              conversationId: conversation.conversationid,
-              id: { gt: lastReadMessageId }, // Selectionne les messages ayant un ID supérieur au dernier message lu
-            },
-          });
-        } else {
-          // Si aucun message n'a été lu, compte tous les messages comme non lus dans cette conversation
-          unreadMessagesCount = await mongoClient.message.count({
+      const conversationsWithMessages = await Promise.all(
+        formatedUserConversations.map(async (conversation) => {
+          // Récupère le dernier message de la conversation courante
+          const lastMessage = await mongoClient.message.findMany({
             where: {
               conversationId: conversation.conversationid,
             },
+            orderBy: {
+              id: 'desc', // Tri par ordre décroissant pour obtenir le dernier message
+            },
+            take: 1, // Prend seulement le premier résultat qui est le dernier message
           });
-        }
 
-        // Récupère la date du dernier message pour l'afficher dans la conversation
-        const lastMessageDate = lastMessage ? getTimestampFromMongoObject(lastMessage) : null;
+          // Recherche le dernier message lu par l'utilisateur dans cette conversation
+          const lastMessageRead = await mongoClient.lastMessageRead.findFirst({
+            where: {
+              conversationId: conversation.conversationid,
+              readerId: user.id, // Filtre par ID de l'utilisateur pour trouver son dernier message lu
+            },
+          });
 
-        if (lastMessage.deleted) {
-          lastMessage.content = 'This message has been deleted';
-        }
+          // Initialise le compteur de messages non lus
+          let unreadMessagesCount = 0;
+          if (lastMessageRead) {
+            // Si un dernier message lu est trouvé, compte les messages non lus depuis ce dernier message
+            const lastReadMessageId = lastMessageRead.messageId;
 
-        // Compte le nombre total de messages dans la conversation
-        const totalMessages = await mongoClient.message.count({
-          where: {
-            conversationId: conversation.conversationid,
-          },
-        });
+            unreadMessagesCount = await mongoClient.message.count({
+              where: {
+                conversationId: conversation.conversationid,
+                id: { gt: lastReadMessageId }, // Selectionne les messages ayant un ID supérieur au dernier message lu
+              },
+            });
+          } else {
+            // Si aucun message n'a été lu, compte tous les messages comme non lus dans cette conversation
+            unreadMessagesCount = await mongoClient.message.count({
+              where: {
+                conversationId: conversation.conversationid,
+              },
+            });
+          }
 
-        // Retourne la conversation enrichie avec le dernier message, le nombre de messages non lus et le nombre total de messages
-        return {
-          ...conversation,
-          lastMessage: { content: lastMessage.content, date: lastMessageDate }, // Récupère le content du message et la date
-          unreadMessagesCount, // Ajoute le nombre de messages non lus à l'objet de la conversation
-          totalMessages, // Ajoute le nombre total de messages à l'objet de la conversation
-        };
-      }));
+          // Récupère la date du dernier message pour l'afficher dans la conversation
+          const lastMessageDate = lastMessage ? getTimestampFromMongoObject(lastMessage) : null;
+
+          if (lastMessage.deleted) {
+            lastMessage.content = 'This message has been deleted';
+          }
+
+          // Compte le nombre total de messages dans la conversation
+          const totalMessages = await mongoClient.message.count({
+            where: {
+              conversationId: conversation.conversationid,
+            },
+          });
+
+          // Retourne la conversation enrichie avec le dernier message, le nombre de messages non lus et le nombre total de messages
+          return {
+            ...conversation,
+            lastMessage: { content: lastMessage.content, date: lastMessageDate }, // Récupère le content du message et la date
+            unreadMessagesCount, // Ajoute le nombre de messages non lus à l'objet de la conversation
+            totalMessages, // Ajoute le nombre total de messages à l'objet de la conversation
+          };
+        }),
+      );
 
       return res.status(200).json(conversationsWithMessages);
     } catch (err) {
@@ -115,11 +117,11 @@ export default {
 
   newConversation: async (req, res, next) => {
     const { user } = res.locals;
-    const { receiverEmail } = req.body;
+    const { receiverId } = req.body;
 
     try {
       const receiver = await postgresClient.user.findUnique({
-        where: { email: receiverEmail },
+        where: { id: receiverId },
       });
 
       if (!receiver) {
@@ -129,18 +131,36 @@ export default {
         });
       }
 
-      const conversation = await postgresClient.conversation.create({
-        data: {
+      // Only one conversation between two users at a time
+      const existingConversation = await postgresClient.conversation.findFirst({
+        where: {
           users: {
-            connect: [
-              { email: user.email },
-              { email: receiver.email },
-            ],
+            every: {
+              id: { in: [user.id, receiver.id] },
+            },
           },
         },
       });
 
-      return res.status(201).json(conversation);
+      if (existingConversation) {
+        return res.json({
+          created: false,
+          ...existingConversation,
+        });
+      }
+
+      const conversation = await postgresClient.conversation.create({
+        data: {
+          users: {
+            connect: [{ email: user.email }, { email: receiver.email }],
+          },
+        },
+      });
+
+      return res.status(201).json({
+        created: true,
+        ...conversation,
+      });
     } catch (err) {
       return next({
         status: 500,
--- src/routers/conversationRouter.js
+++ src/routers/conversationRouter.js
@@ -86,17 +86,37 @@ router.get('/', conversationController.getConversations);
  *           schema:
  *             type: object
  *             properties:
- *               receiverEmail:
- *                 type: string
+ *               receiverId:
+ *                 type: integer
  *     responses:
- *       '201':
+ *       200:
+ *         description: Conversation already exists
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 created:
+ *                   type: boolean
+ *               example:
+ *                 id: 1
+ *                 created: false
+ *       201:
  *         description: New conversation created
  *         content:
  *           application/json:
  *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/message'
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 created:
+ *                   type: boolean
+ *               example:
+ *                 id: 1
+ *                 created: true
  *       401:
  *         description: Unauthorized
  *       404:
